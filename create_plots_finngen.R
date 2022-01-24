2ff6c8f9c0654b47a96a3f0a54c987a5218e2412
Andrea Ganna <andreaganna@MacBook-Pro-3.local>
2022-01-24 16:26:28 -0500

    deleted R5

--- create_plots_finngen.R
+++ create_plots_finngen.R
@@ -11,7 +11,7 @@ library(bumphunter) #BiocManager::install("bumphunter")
 library(org.Hs.eg.db) #BiocManager::install("org.Hs.eg.db")
 
 
-pheno <- fread("data/finngen_R5_pheno_n.tsv")
+pheno <- fread("data/finngen_R6_pheno_n.tsv")
 
 wrapper <- function(x, ...) 
 {
@@ -29,9 +29,9 @@ set.seed(123)
 RES <- NULL
 for (i in pheno$phenocode[pheno$num_gw_significant>0])
 {
-    system(paste0("gsutil cp gs://finngen-public-data-r5/summary_stats/finngen_R5_" ,i,".gz ."))
+    system(paste0("gsutil cp gs://finngen-public-data-r6/summary_stats/finngen_R6_" ,i,".gz ."))
   
-	df1 <- fread(cmd=paste0("gunzip -c finngen_R5_",i,".gz"), header=T, sep="\t", select=c('#chrom', 'pos', 'pval')) 
+	df1 <- fread(cmd=paste0("gunzip -c finngen_R6_",i,".gz"), header=T, sep="\t", select=c('#chrom', 'pos', 'pval')) 
 	
 	if(min(df1$pval,na.rm=T)<0.00000005)
 	{
@@ -103,7 +103,7 @@ for (i in pheno$phenocode[pheno$num_gw_significant>0])
 	   RES <- rbind(RES,cbind(pheno[pheno$phenocode==i,c("phenocode","name","num_cases","num_controls")],pheno$path_bucket[pheno$phenocode==i], paste0(i,"_MF.png"), paste0("http://r5.finngen.fi/pheno/",i),paste0("https://r5.risteys.finngen.fi/phenocode/",i)))
 	}
 
-    system(paste0("rm finngen_R5_",i,".gz"))
+    system(paste0("rm finngen_R6_",i,".gz"))
 
     print(which(i==pheno$phenocode))
 }
